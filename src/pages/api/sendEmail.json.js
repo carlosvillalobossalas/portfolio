057bbcfeafe96383dd9d5a1eb3d839f1b0571238
carlosvillalobossalas <carlosvillalobos247@gmail.com>
2024-03-23 13:59:49 -0600

    email function changed

--- src/pages/api/sendEmail.json.js
+++ src/pages/api/sendEmail.json.js
@@ -1,45 +1,77 @@
-import { Resend } from "resend";
+// import { Resend } from "resend";
+import nodemailer from "nodemailer";
 
-const resend = new Resend(import.meta.env.RESEND_API_KEY);
-
+// const resend = new Resend(import.meta.env.RESEND_API_KEY);
+const googlePassword = import.meta.env.GOOGLE_PASSWORD;
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: "carlosvillalobos247@gmail.com",
+    pass: googlePassword,
+  },
+});
 export const POST = async ({ params, request }) => {
   const body = await request.json();
   const { from, subject, html } = body;
-  const send = await resend.emails.send({
-    from: from,
-    to: "carlosvillalobos247@gmail.com",
-    subject: subject,
-    html: html,
-  });
+  try {
+    await transporter.verify();
 
-  if (send.data) {
+    // const send = await resend.emails.send({
+    //   from: "jmonge@jpc.fi.cr",
+    //   to: "carlosvillalobos247@gmail.com",
+    //   subject: subject,
+    //   html: html,
+    // });
+
+    const res = await transporter.sendMail({
+      from: from,
+      to: "carlosvillalobos247@gmail.com",
+      subject: `${subject} ${from}`,
+      html: `<p>${html}</p>`,
+      text: html,
+    });
+
+    // console.log(res);
+
+    // if (send.data) {
+    //   return new Response(
+    //     JSON.stringify({
+    //       message: send.data,
+    //     }),
+    //     {
+    //       status: 200,
+    //       statusText: "OK",
+    //     }
+    //   );
+    // }
+
+    // if (send.error) {
+    //   console.log(send.error);
+    //   return new Response(
+    //     JSON.stringify({
+    //       message: send.error,
+    //     }),
+    //     {
+    //       status: 500,
+    //       statusText: "Internal Server Error",
+    //     }
+    //   );
+    // }
+
     return new Response(
       JSON.stringify({
-        message: send.data,
-      }),
-      {
-        status: 200,
-        statusText: "OK",
-      }
+        name: "Astro",
+        url: "https://astro.build/",
+      })
     );
-  }
-
-  if (send.error) {
+  } catch (error) {
     return new Response(
       JSON.stringify({
-        message: send.error,
-      }),
-      {
-        status: 500,
-        statusText: "Internal Server Error",
-      }
+        name: "Astro",
+        url: "https://astro.build/",
+      })
     );
   }
-
-  return new Response(
-    JSON.stringify({
-      name: "Astro",
-      url: "https://astro.build/",
-    })
-  );
 };
