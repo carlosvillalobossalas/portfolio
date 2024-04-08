f4d5a64012847cae2241a4214ffd6bd37faddf76
carlosvillalobossalas <carlosvillalobos247@gmail.com>
2024-04-08 17:26:57 -0600

    i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,62 @@
+export const languages = {
+  en: "English",
+  es: "Spanish",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.experience": "Experience",
+    "nav.projects": "Projects",
+    "nav.certifications": "Certifications",
+    "nav.contact": "Contact",
+    "about.role": "Software Developer",
+    "about.description": `+3 years of experience. 
+    Graduated in computer engineering from the Costa Rica Institute of Technology. 
+    Knowledge of web, server, mobile and database technologies.`,
+    "experiencie.container.title": "Professional experience",
+    "experiencie.fullstack": "Full Stack Developer",
+    "experiencie.fresh.description":
+      "In charge of resolving tickets in Jira to solve errors and add or remove functionalities on the client's different platforms",
+    "experience.ff.description":
+      "Responsible for complying with the delivery of tickets to improve and expand the operation of the client's different web applications.",
+    "experiencie.soin.description":
+      "Creation of a complete web system for the QA department. Features such as daily reporting, metrics execution, user management and surveys were included.",
+
+    "projects.container.title": "Projects",
+    "projects.cerezo.description":
+      "Agenda that Cerezo workers use daily to take orders and print them using a thermal printer. Within the application you can manage users, clients and products.",
+    "projects.condo.description":
+      "Platform under development for users who want to manage their condominiums. You can register properties, send notifications, create logs, create reservations and manage services.",
+    "certifications.container.title": "Certifications",
+    "contact.container.title": "Contact",
+  },
+  es: {
+    "nav.experience": "Experiencia",
+    "nav.projects": "Proyectos",
+    "nav.certifications": "Certificaciones",
+    "nav.contact": "Contacto",
+    "about.role": "Desarrollador de Software",
+    "about.description": `+3 años de experiencia. 
+    Graduado en ingeniería en computación en el Instituto Tecnológico de Costa Rica. 
+    Conocimientos en tecnologías web, de servidor, móviles y de bases de datos.`,
+    "experiencie.container.title": "Experiencia laboral",
+    "experiencie.fullstack": "Desarrollador Full Stack",
+    "experiencie.fresh.description":
+      "Encargado de resolver tickets en Jira para solucionar errores y agregar o eliminar funcionalidades en las diferentes plataformas del cliente.",
+    "experience.ff.description":
+      "Responsable de cumplir con la entrega de tickets para mejorar y expandir el funcionamiento de las diferentes aplicaciones web del cliente.",
+    "experiencie.soin.description":
+      "Creación de un sistema web completo para el departamento de QA. Se incluyeron funcionalidades como creación de reportes diarios, ejecución de métricas, administración de usuarios y encuestas.",
+
+    "projects.container.title": "Projectos",
+    "projects.cerezo.description":
+      "Agenda que utilizan los trabajadores de Cerezo cada día para tomar pedidos e imprimirlos utilizando una impresora térmica. Dentro de la aplicación se pueden administrar los usuarios, los clientes y los productos.",
+    "projects.condo.description":
+      "Plataforma en desarrollo para usuarios que quieran administrar sus condominios. Se pueden registrar propiedades, enviar notificaciones, crear bitácoras, crear reservaciones y administrar servicios.",
+
+    "certifications.container.title": "Certificaciones",
+    "contact.container.title": "Contacto",
+  },
+} as const;
