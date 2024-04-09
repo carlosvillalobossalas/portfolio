cbd954f4c7304b3defdcef9e7c5f6c25f331d582
carlosvillalobossalas <carlosvillalobos247@gmail.com>
2024-04-08 18:15:39 -0600

    i18n

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -15,6 +15,7 @@ export const ui = {
     "about.description": `+3 years of experience. 
     Graduated in computer engineering from the Costa Rica Institute of Technology. 
     Knowledge of web, server, mobile and database technologies.`,
+    //!Experience
     "experiencie.container.title": "Professional experience",
     "experiencie.fullstack": "Full Stack Developer",
     "experiencie.fresh.description":
@@ -23,14 +24,44 @@ export const ui = {
       "Responsible for complying with the delivery of tickets to improve and expand the operation of the client's different web applications.",
     "experiencie.soin.description":
       "Creation of a complete web system for the QA department. Features such as daily reporting, metrics execution, user management and surveys were included.",
-
+    //!Projects
     "projects.container.title": "Projects",
     "projects.cerezo.description":
       "Agenda that Cerezo workers use daily to take orders and print them using a thermal printer. Within the application you can manage users, clients and products.",
     "projects.condo.description":
       "Platform under development for users who want to manage their condominiums. You can register properties, send notifications, create logs, create reservations and manage services.",
+    //!Certifications
     "certifications.container.title": "Certifications",
+    "certifications.openai.title":
+      "OpenAI: Practical exercises and assistants with React + NestJS",
+    "certifications.openai.description":
+      "Created a server with NestJS that consumes the OpenAI library and consumes it from a website created with React. The app's functionalities include image generation and editing, text-based audio generation, text to audio, setting up an assistant, and more.",
+    "certifications.docker.title":
+      "Docker - Practical user guide for developers",
+    "certifications.docker.description":
+      "Knowledge is acquired to use, create and configure images on different architectures and deploy them to cloud records.",
+    "certifications.flutter.title":
+      "Flutter - Mobile: From Zero to Expert - 2023 Edition",
+    "certifications.flutter.description":
+      "It provides the opportunity to develop any type of application on your own.",
+    "certifications.reactPro.title":
+      "React PRO: take your foundation to the next level",
+    "certifications.reactPro.description":
+      "The goal of the course is to improve existing React skills.",
+    "certifications.reactSocket.title":
+      "React: Real-time applications with Socket-io",
+    "certifications.reactSocket.description":
+      "Learn to create applications that interact with each other simultaneously and instantly.",
+    "certifications.reactZero.title":
+      "React: From zero to expert (Hooks and MERN)",
+    "certifications.reactZero.description":
+      "You gain a lot of knowledge about React from scratch, including unit and integration testing.",
+    "certifications.see.credential": "See credential",
+    //!Contact
     "contact.container.title": "Contact",
+    "contact.subject": "Subject",
+    "contact.message": "Message",
+    "contact.send": "Send",
   },
   es: {
     "nav.experience": "Experiencia",
@@ -55,8 +86,36 @@ export const ui = {
       "Agenda que utilizan los trabajadores de Cerezo cada día para tomar pedidos e imprimirlos utilizando una impresora térmica. Dentro de la aplicación se pueden administrar los usuarios, los clientes y los productos.",
     "projects.condo.description":
       "Plataforma en desarrollo para usuarios que quieran administrar sus condominios. Se pueden registrar propiedades, enviar notificaciones, crear bitácoras, crear reservaciones y administrar servicios.",
-
+    //!CERTIFICATIONS
     "certifications.container.title": "Certificaciones",
+    "certifications.openai.title":
+      "OpenAI: Ejercicios prácticos y asistentes con React + NestJS",
+    "certifications.openai.description":
+      "Creación de un servidor con NestJS que consume la librería de OpenAI y este se consume desde un sitio web creado con React. Entre las funcionalidades de la aplicación está la generación y edición de imágenes, generación de audio basado en texto, texto a audio, configurar un asistente y más.",
+    "certifications.docker.title":
+      "Docker - Guía práctica de uso para desarrolladores",
+    "certifications.docker.description":
+      "Se adquieren conocimientos para utilizar, crear y configurar imágenes en diferentes arquitecturas y desplegarlas en registros en la nube.",
+    "certifications.flutter.title":
+      "Flutter - Móvil: De cero a experto - Edición 2023",
+    "certifications.flutter.description":
+      "Brinda la oportunidad de desarrollar cualquier tipo de aplicación por su cuenta.",
+    "certifications.reactPro.title":
+      "React PRO: Lleva tus bases al siguiente nivel",
+    "certifications.reactPro.description":
+      "El objetivo del curso es mejorar las habilidades existentes de React.",
+    "certifications.reactSocket.title":
+      "React: Aplicaciones en tiempo real con Socket-io",
+    "certifications.reactSocket.description":
+      "Aprender a crear aplicaciones que interactúen entre si de manera simultánea e instantánea.",
+    "certifications.reactZero.title": "React: De cero a experto (Hooks y MERN)",
+    "certifications.reactZero.description":
+      "Se adquieren desde cero muchos conocimientos sobre React, incluyendo pruebas unitarias y de integración.",
+    "certifications.see.credential": "Ver credencial",
+    //!CONTACT
     "contact.container.title": "Contacto",
+    "contact.subject": "Asunto",
+    "contact.message": "Mensaje",
+    "contact.send": "Enviar",
   },
 } as const;
